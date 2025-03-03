5e839cb14abd70bad453e8762c294afabf83dd02
vikrant221502 <75710670+vikrant221502@users.noreply.github.com>
2025-03-03 21:27:40 +0530

    app.py

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def clone_voice():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=True)
